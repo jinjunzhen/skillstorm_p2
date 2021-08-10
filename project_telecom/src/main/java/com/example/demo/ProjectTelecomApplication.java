4acab46766bd7e625d8bbec8bf626373311d44c1
jinjun zhen <jinjunzhen@yahoo.com>
2021-08-10 08:24:30 -0500

    first commet

---
+++ project_telecom/src/main/java/com/example/demo/ProjectTelecomApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTelecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTelecomApplication.class, args);
+		System.out.println("ok, we r gonna start our project here");
+	}
+
+}
