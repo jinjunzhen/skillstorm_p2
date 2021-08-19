45225e9f8396ee15ccc938336aa93ac0a6f55317
jinjun zhen <jinjunzhen@yahoo.com>
2021-08-18 21:30:56 -0500

    remove telecom-ui

--- telecom-ui/telecom-angular/src/app/models/Phone.ts
+++
@@ -1,8 +0,0 @@
-class Phone {
-    phone_id! : number;
-    phone_area! : number;
-    phone_first_name!: string;
-    phone_last_name!: string;
-    phone_number!: number
-}
-export default Phone;
--- telecom-ui/telecom-angular/src/app/models/Plan.ts
+++
@@ -1,5 +0,0 @@
-class Plan{
-    plan_id!: number;
-    plan_type! : string;
-}
-export default Plan;
